783cbe8ce2f8875bf5a725d34ee9d007012dc68b
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-11-24 22:20:03 +0100

    feature(follow): implement unfollow user method

--- src/followers/services/follow_service.py
+++ src/followers/services/follow_service.py
@@ -35,3 +35,28 @@ class FollowService:
         else:
             logging.info(f"User {follower.username} is already following {followed.username}.")
             raise ValidationError({"error": f"User {follower.username} is already following {followed.username}."})
+
+    @staticmethod
+    def unfollow_user(follower: User, followed: User):
+        """
+        Deletes a follow relationship between two users.
+
+        :param follower: The user who is unfollowing.
+        :param followed: The user being unfollowed.
+        :return: True if the unfollow relationship was successful, False otherwise.
+        """
+        # Ensure a valid follower and followed are provided
+        if follower.cognito_id == followed.cognito_id:
+            logging.warning(f"User {follower.username} attempted to unfollow themselves.")
+            raise ValidationError({"error": "You cannot unfollow yourself."})
+
+        # Attempt to delete the follow relationship
+        deleted_count, _ = Follow.objects.filter(follower=follower, followed=followed).delete()
+
+        if deleted_count > 0:
+            logging.info(f"User {follower.username} successfully unfollowed {followed.username}.")
+            return True
+        else:
+            logging.warning(f"User {follower.username} attempted to unfollow {followed.username}, but no follow relationship existed.")
+            raise ValidationError({"error": f"You are not following {followed.username}."})
+
