883386c61938facc21ef771421da1757541054a6
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-11-24 22:20:03 +0100

    feature(follow): implement unfollow user method

--- src/followers/urls.py
+++ src/followers/urls.py
@@ -4,4 +4,5 @@ from followers import views
 
 urlpatterns = [
     path("follow", views.follow_user, name="follow"),
+    path("unfollow", views.unfollow_user, name="unfollow"),
 ]
--- src/followers/views.py
+++ src/followers/views.py
@@ -48,3 +48,45 @@ def follow_user(request):
     follow_service.follow_user(follower, followed)
 
     return Response(status=status.HTTP_201_CREATED)
+
+
+@api_view(["POST"])
+def unfollow_user(request):
+    user_id = request.query_params.get("user_id")
+
+    if not user_id:
+        return Response({"error": "Missing 'user_id' query parameter."}, status=status.HTTP_400_BAD_REQUEST)
+
+    access_token = request.COOKIES.get("access_token")
+    if not access_token:
+        return Response({"error": "Unauthorized access."}, status=status.HTTP_401_UNAUTHORIZED)
+
+    # Decode token to get the current user
+    try:
+        token_service = TokenService()
+        user_info = token_service.decode_token(access_token)
+    except PyJWTError:
+        return Response(
+            {"error": "Invalid or expired access token."},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    # Retrieve the follower and followed user objects
+    user_service = UserService()
+    follower = user_service.get_user_by_cognito_id(user_info["username"])
+    if not follower:
+        return Response(
+            {"error": f"Authenticated user with Cognito ID {user_info['username']} not found."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    followed = user_service.get_user_by_cognito_id(user_id)
+    if not followed:
+        return Response(
+            {"error": f"User with ID {user_id} not found."},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    follow_service = FollowService()
+    follow_service.unfollow_user(follower, followed)
+    return Response({"message": f"Successfully unfollowed {followed.username}."}, status=status.HTTP_200_OK)
