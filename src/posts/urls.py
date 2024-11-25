83092212df628376ba5c7d1a3b00e37dfceec156
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-11-25 01:24:47 +0100

    feature(post): implement delete post - change crate post url path

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path("post/", views.create_post, name="create_post"),
+    path("post/create", views.create_post, name="create_post"),
+    path("post/delete", views.delete_post, name="delete_post"),
 ]
